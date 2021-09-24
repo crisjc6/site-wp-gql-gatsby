13f36b678f94811bd6b8074b91464f4036d76242
Cristhian Jumbo <sonclonmenax@gmail.com>
2021-09-24 12:24:00 -0500

    some feedback

---
+++ src/components/Navbar.js
@@ -0,0 +1,230 @@
+import React from 'react'
+import { Link, StaticQuery, graphql } from 'gatsby'
+import github from '../img/github-icon.svg'
+import logo from '../img/logo.svg'
+const Navbar = class extends React.Component {
+        constructor(props) {
+                super(props)
+                this.state = {
+                        active: false,
+                        navBarActiveClass: '',
+                }
+        }
+
+        toggleHamburger = () => {
+                // toggle the active boolean in the state
+                this.setState(
+                    {
+                            active: !this.state.active,
+                    },
+                    // after state has been updated,
+                    () => {
+                            // set the class in state for the navbar accordingly
+                            this.state.active
+                                ? this.setState({
+                                        navBarActiveClass: 'is-active',
+                                })
+                                : this.setState({
+                                        navBarActiveClass: '',
+                                })
+                    }
+                )
+        }
+        render() {
+                return (
+    <StaticQuery
+        query={graphql`
+      query {
+        allWordpressPage(sort: { fields: wordpress_id }, limit: 5) {
+          edges {
+            node {
+              title
+              slug
+            }
+          }
+        }
+      }
+    `}
+                        render={data => {
+                                return (
+                                    <nav
+                                        className="navbar is-transparent"
+                                        role="navigation"
+                                        aria-label="main-navigation"
+                                    >
+                                        <div className="container flex-around">
+                                            <div className="navbar-brand logo">
+                                                <Link to="/" className="navbar-item">
+                                                    <figure className="image">
+                                                        <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+                                                    </figure>
+                                                </Link>
+                                                {/* Hamburger menu */}
+                                                <div
+                                                    className={`navbar-burger burger ${this.state.navBarActiveClass}`}
+                                                    data-target="navMenu"
+                                                    onClick={() => this.toggleHamburger()}
+                                                    onKeyDown={() => this.toggleHamburger()}
+                                                    role="button" tabIndex="0"
+                                                >
+                                                    <span />
+                                                    <span />
+                                                    <span />
+                                                </div>
+                                            </div>
+                                            <div
+                                                id="navMenu"
+                                                className={`navbar-menu ${this.state.navBarActiveClass}`}
+                                            >
+                                                <div className="navbar-start has-text-centered">
+                                                    <Link className="navbar-item" to="/blog">
+                                                        Home
+                                                    </Link>
+                                                    <Link className="navbar-item" to="/blog">
+                                                        Features
+                                                    </Link>
+                                                    <Link className="navbar-item" to="/about">
+                                                        Team
+                                                    </Link>
+                                                    <div className="navbar-item has-dropdown is-hoverable">
+                                                        <Link className="navbar-item" to="/about">
+                                                            Blog
+                                                        </Link>
+                                                        <div className="navbar-dropdown">
+                                                            <Link className="navbar-item" to="/about">
+                                                                Blog Detail
+                                                            </Link>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="navbar-end">
+                                                    <div className="navbar-item">
+                                                        <div className="buttons">
+                                                            <a className="button is-primary">
+                                                                <strong>
+                                                                    Sign up
+                                                                </strong>
+                                                            </a>
+                                                            <a className="button is-light">
+                                                                Log in
+                                                            </a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </nav>
+                                    // <nav className="navbar is-transparent">
+                                    //         <div className="container">
+                                    //                 <div className="navbar-brand">
+                                    //                         <Link to="/" className="navbar-item">
+                                    //                                 <figure className="image">
+                                    //                                         <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+                                    //                                 </figure>
+                                    //                         </Link>
+                                    //                 </div>
+                                    //                 <div
+                                    //                     className={`navbar-burger burger ${this.state.navBarActiveClass}`}
+                                    //                     data-target="navMenu"
+                                    //                     onClick={() => this.toggleHamburger()}
+                                    //                     onKeyDown={() => this.toggleHamburger()}
+                                    //                     role="button" tabIndex="0"
+                                    //                 >
+                                    //                         <span />
+                                    //                         <span />
+                                    //                         <span />
+                                    //                 </div>
+                                    //                 <div className="navbar-start">
+                                    //                         {data.allWordpressPage.edges.map(edge => (
+                                    //                             <Link
+                                    //                                 className="navbar-item"
+                                    //                                 to={edge.node.slug}
+                                    //                                 key={edge.node.slug}
+                                    //                             >
+                                    //                                     {edge.node.title}
+                                    //                             </Link>
+                                    //                         ))}
+                                    //                 </div>
+                                    //                 <div className="navbar-end">
+                                    //                         <div className="navbar-item">
+                                    //                                 <div className="buttons">
+                                    //                                         <a className="button is-primary">
+                                    //                                                 <strong>
+                                    //                                                         Sign up
+                                    //                                                 </strong>
+                                    //                                         </a>
+                                    //                                         <a className="button is-light">
+                                    //                                                 Log in
+                                    //                                         </a>
+                                    //                                 </div>
+                                    //                         </div>
+                                    //                 </div>
+                                    //         </div>
+                                    // </nav>
+                                )
+                        }}
+                    />
+                );
+        }
+}
+
+export default Navbar
+
+
+
+// const Navbar = () => (
+//     <StaticQuery
+//         query={graphql`
+//       query {
+//         allWordpressPage(sort: { fields: wordpress_id }, limit: 5) {
+//           edges {
+//             node {
+//               title
+//               slug
+//             }
+//           }
+//         }
+//       }
+//     `}
+//
+//         render={data => (
+//             <nav className="navbar is-transparent" id="my-nav">
+//                     <div className="container">
+//                             <div className="navbar-brand">
+//                                     <Link to="/" className="navbar-item">
+//                                             <figure className="image">
+//                                                     <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+//                                             </figure>
+//                                     </Link>
+//                             </div>
+//                             <div className="navbar-start">
+//                                     {data.allWordpressPage.edges.map(edge => (
+//                                         <Link
+//                                             className="navbar-item"
+//                                             to={edge.node.slug}
+//                                             key={edge.node.slug}
+//                                         >
+//                                                 {edge.node.title}
+//                                         </Link>
+//                                     ))}
+//                             </div>
+//                             <div className="navbar-end">
+//                                     <div className="navbar-item">
+//                                             <div className="buttons">
+//                                                     <a className="button is-primary">
+//                                                             <strong>
+//                                                                     Sign up
+//                                                             </strong>
+//                                                     </a>
+//                                                     <a className="button is-light">
+//                                                             Log in
+//                                                     </a>
+//                                             </div>
+//                                     </div>
+//                             </div>
+//                     </div>
+//             </nav>
+//         )}
+//     />
+// )
+// export default Navbar
---
+++ src/templates/blog.js
@@ -0,0 +1,232 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import PostList from '../components/PostList'
+import Pagination from '../components/Pagination'
+import home from '../img/home.svg'
+import shape from '../img/shape.svg'
+import client from '../img/client.svg'
+import feature1 from '../img/feauture1.svg'
+import feature2 from '../img/feature2.svg'
+import feature3 from '../img/feature2.svg'
+import descri01 from '../img/descri01.svg'
+import descri02 from '../img/descri02.svg'
+import descri03 from '../img/descri03.svg'
+
+export default class IndexPage extends React.Component {
+  render() {
+    const { data, pageContext } = this.props
+    const { edges: posts } = data.allWordpressPost
+
+    return (
+        <Layout>
+          <section className="hero ">
+            <div className="hero-body">
+              <div className="container">
+                <div className="columns">
+                  <div className="column  is-4">
+                    <p className="title">
+                      Get your IP done right with Patent P.C.
+                    </p>
+                    <p className="subtitle">
+                      Patent Professional Corporation offers the most comprehensive fixed-fee intellectual property protection for growing businesses.
+                    </p>
+                    <button className="button is-primary">
+                      Get started
+                    </button>
+                  </div>
+                  <div className="column  is-8">
+                    <figure className="image is-4by3">
+                      <img src={home} alt="Kaldi" style={{ width: '350px' }} />
+                    </figure>
+                  </div>
+                </div>
+                <div className="columns">
+                  <div className="column has-text-centered is-12">
+                    <p className="subtitle">
+                      Our lawyers have worked on IP protection for
+                      (logos for Intel, Applied Materials, Adobe, Shutterfly, Align Tech, Compaq, Univ Calif Berkeley, Stanford in addition to the list in the current page that hidden
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <figure className="image is-4by3">
+            <img src={shape} alt="Kaldi"/>
+          </figure>
+          <section className="hero"  style={{
+            backgroundColor: "#F9FAFC",
+          }}>
+            <div className="hero-body">
+              <div className="container">
+                <div className="columns">
+                  <div className="column is-12">
+                    <p className="title">
+                      Get your IP done right with Patent P.C.
+                    </p>
+                    <p className="subtitle">
+                      Patent Professional Corporation offers the most comprehensive fixed-fee intellectual property protection for growing businesses.
+                    </p>
+                  </div>
+                </div>
+                <div className="columns is-vcentered" >
+                  <div className="column" >
+                    <figure className="image">
+                      <img src={client} alt="Kaldi" style={{ width: '200px' }}/>
+                    </figure>
+                  </div>
+                </div>
+                <div className="columns">
+                  <div className="column is-12 has-text-centered">
+                    <p className="title">
+                      Why choose us?
+                    </p>
+                  </div>
+                </div>
+                <nav className="columns box">
+                  <a className="bd-focus-item column has-text-centered">
+                    <figure className="bd-focus-icon">
+                      <img src={feature1} alt="Kaldi" style={{ width: '100px' }}/>
+                    </figure>
+                    <p className="">
+                      <strong>
+                        TRUSTED PROFESSIONALS
+                      </strong>
+                    </p>
+                    <p className="subtitle is-6">
+                      Our patent lawyers and agents came from large law firms and have worked with many Fortune 500 companies.
+                    </p>
+                  </a>
+                  <a className="bd-focus-item column has-text-centered">
+                    <figure className="bd-focus-icon">
+                      <img src={feature2} alt="Kaldi" style={{ width: '100px' }}/>
+                    </figure>
+                    <p className="">
+                      <strong>
+                        ADVANCED TECHNOLOGY
+                      </strong>
+                    </p>
+                    <p className="subtitle is-6">
+                      We not only understand technology, but we also developed AI software to improve our IP workflow and pass the savings to you.
+                    </p>
+                  </a>
+                  <a className="bd-focus-item column has-text-centered">
+                    <figure className="bd-focus-icon">
+                      <img src={feature3} alt="Kaldi" style={{ width: '100px' }}/>
+                    </figure>
+                    <p className="">
+                      <strong>
+                        FIXED-FEE PRICING
+                      </strong>
+                    </p>
+                    <p className="subtitle is-6">
+                      Our tech enables your disclosure to be efficiently converted to patents. Our cost-effective flat-rate pricing ensures that legal bill surprises are not in your future.
+                    </p>
+                  </a>
+                  <a className="bd-focus-item column has-text-centered">
+                    <figure className="bd-focus-icon">
+                      <img src={feature1} alt="Kaldi" style={{ width: '100px' }}/>
+                    </figure>
+                    <p className="">
+                      <strong>
+                        PERSONALIZED SERVICE
+
+                      </strong>
+                    </p>
+                    <p className="subtitle is-6">
+                      Get fast, responsive service. Your dedicated lawyer knows your business from the ground up.
+                    </p>
+                  </a>
+
+                </nav>
+              </div>
+            </div>
+          </section>
+          <section className="hero is-fullheight is-default is-bold">
+            <div className="hero-body">
+              <div className="container has-text-centered">
+                <div className="columns is-vcentered">
+                  <div className="column is-5">
+                    <figure className="image is-4by3">
+                      <img src={descri01} alt="Kaldi"/>
+                    </figure>
+                  </div>
+                  <div className="column is-6 is-offset-1 is-vcentered">
+                    <h2 className="subtitle is-4 has-text-centered">
+                      Our results are almost 2x better than those of other law firms
+                      For clients who wants to go all-the-way, our historical allowance rate has been 95%. This compares to the 55% success rate for the average US law firm. Our work is meticulously done and our diagnostics and analytics produces better results, and this shows in our past successes.
+                    </h2>
+                    <br />
+                  </div>
+                </div>
+              </div>
+              <div className="container has-text-centered">
+                <div className="columns is-vcentered">
+                  <div className="column is-6 is-offset-1 is-vcentered">
+                    <h2 className="subtitle is-4 has-text-centered">
+                      You won’t be surprised by our fixed-fees
+                      Our services are priced on a flat-fee basis, so you can budget for all your IP requirements. Others charge by the hour, and you won’t find out until they send you an invoice.  You now have control over your IP spending.
+                    </h2>
+                    <br />
+                  </div>
+                  <div className="column is-5">
+                    <figure className="image is-4by3">
+                      <img src={descri02} alt="Kaldi"/>
+                    </figure>
+                  </div>
+                </div>
+              </div>
+              <div className="container has-text-centered">
+                <div className="columns is-vcentered">
+                  <div className="column is-5">
+                    <figure className="image is-4by3">
+                      <img src={descri03} alt="Kaldi"/>
+                    </figure>
+                  </div>
+                  <div className="column is-6 is-offset-1 is-vcentered">
+                    <h2 className="subtitle is-4 has-text-centered">
+                      Your IP first draft done in days. Not in months.
+                      With first-to-file rules, time is of the essence.  From your disclosure, we can prepare first drafts in a few days with our tech-savvy professionals and our patented system.  You’ll be amazed with our responsiveness.
+                    </h2>
+                    <br />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <PostList posts={posts} title="Latest posts" />
+          <Pagination pageContext={pageContext} pathPrefix="/" />
+        </Layout>
+    )
+  }
+}
+
+IndexPage.propTypes = {
+  data: PropTypes.shape({
+    allWordpressPost: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }),
+  pageContext: PropTypes.shape({
+    currentPage: PropTypes.number,
+    numPages: PropTypes.number,
+  }),
+}
+
+export const pageQuery = graphql`
+  query IndexQuery($limit: Int!, $skip: Int!) {
+    allWordpressPost(
+      sort: { fields: date, order: DESC }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          ...PostListFields
+        }
+      }
+    }
+  }
+`
